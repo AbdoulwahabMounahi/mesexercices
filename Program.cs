8404ef01d2d25a9479571470d759c9675eb7394f
AbdoulwahabMounahi <abdoulwahabmounahi200@gmail.com>
2025-10-22 22:22:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace exercice3Tp2;
+
+class Program
+{
+    static int somme(int a, int b)
+    {
+        return a + b;
+    }
+    static void Tableau()
+    {
+        int taille;
+        int sum=0;
+
+        Console.WriteLine("donner la taille du tableau:");
+        taille = int.Parse(Console.ReadLine());
+        int[] tab = new int[taille];
+        for(int i = 0; i < taille; i++)
+        {
+            Console.WriteLine("donner un entier:");
+            tab[i] = int.Parse(Console.ReadLine());
+            sum += tab[i];
+        }
+        Console.WriteLine("la somme des elements du tableau est :"+sum);
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("donner un entier :");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("donner un autre entier :");
+        int b = int.Parse(Console.ReadLine());
+        int resultat=somme(a,b);
+        Console.WriteLine("la somme est :" + resultat);
+        
+        Tableau();
+
+      
+
+    }
+}
