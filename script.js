98bf3192434aa746a741d6f765025dfd8d2dce1f
AbdoulwahabMounahi <abdoulwahabmounahi200@gmail.com>
2025-10-22 22:26:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const nav=document.querySelector("header.header");
-window.addEventListener("scroll",()=>{
- console.log(window.scrollY);
-  if(window.scrollY>120){
-     nav.style.top=0;
-  }
-  else{
-    nav.style.top="-50px";
-  }
-});
+function showForm(formId){
+    document.querySelectorAll(".form-box").forEach(form=>form.classList.remove("active"));
+    document.getElementById(formId).classList.add("active");
+}
